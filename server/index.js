69b408b4ba0761bf8a03e736784475a7c3a82602
Derrick Cross <decross1@me.com>
2018-02-04 21:01:11 -0800

    Additional Changes to DB queries, close to final

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser')
+const router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const AWS = require('aws-sdk');
 const path = require('path');
@@ -15,9 +16,27 @@ AWS.config.loadFromPath(path.resolve(__dirname, '../config.json'));
 // Turn on Body Parsing
 app.use(bodyParser());
 
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
+
+// to map a single object to send to pricing. 
+// db.getAvgSurge(cities.cities).then(results => { 
+//   var test = results.map((obj) => obj.rows[0]);
+//   console.log(test);
+// });
+
+// db.insertAvgSurge();
+
+// var test = data.generateRandomPricingLog();
+// db.insertPricingLogs(test.userId, test.city, test.surgeMultiplier, test.price, test.priceTimestamp);
+
+
+
 // Logic for Creating a new queue;
 let sqs = new AWS.SQS({
-    apiVersion: '2012-11-05'
+  apiVersion: '2012-11-05'
 });
 
 // let params = {
@@ -74,7 +93,7 @@ let deleteMessage = (receiptId, queueUrl) => {
     QueueUrl: queueUrl, 
     ReceiptHandle: receiptId
   }
-  
+
   return new Promise((resolve, reject) => {
     sqs.deleteMessage(params, (err, data) => {
       if(err) {
@@ -85,18 +104,3 @@ let deleteMessage = (receiptId, queueUrl) => {
     });
   });
 };
-
-const server = app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
-});
-
-// to map a single object to send to pricing. 
-// db.getAvgSurge(cities.cities).then(results => { 
-//   var test = results.map((obj) => obj.rows[0]);
-//   console.log(test);
-// });
-
-// db.insertAvgSurge();
-
-// var test = data.generateRandomPricingLog();
-// db.insertPricingLogs(test.userId, test.city, test.surgeMultiplier, test.price, test.priceTimestamp);
